88c7c397fd7ef842e23030d267af5955ef5a1d18
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2016-11-12 19:20:36 +0100

    Uploading already done files

---
+++ scripts/04-3dobjects.js
@@ -0,0 +1,510 @@
+// Global variable definitionvar canvas;
+var gl;
+var shaderProgram;
+
+// Buffers
+var pyramidVertexPositionBuffer;
+var pyramidVertexColorBuffer;
+var cubeVertexPositionBuffer;
+var cubeVertexColorBuffer;
+var cubeVertexIndexBuffer;
+
+
+/*var heroVertexPositionBuffer;
+var heroVertexIndexBuffer;*/
+
+var currentlyPressedKeys = {};
+var speedX = 0;
+var speedY = 0;
+var xPos = 0;
+var yPos = 0;
+
+var hero;
+var world;
+
+// Model-view and projection matrix and model-view matrix stack
+var mvMatrixStack = [];
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+// Variables for storing curent rotation of pyramid and cube
+var rotationPyramid = 0;
+var rotationCube = 0;
+
+// Helper variable for animation
+var lastTime = 0;
+
+//
+// Matrix utility functions
+//
+// mvPush   ... push current matrix on matrix stack
+// mvPop    ... pop top matrix from stack
+// degToRad ... convert degrees to radians
+//
+function mvPushMatrix() {
+  var copy = mat4.create();
+  mat4.set(mvMatrix, copy);
+  mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix() {
+  if (mvMatrixStack.length == 0) {
+    throw "Invalid popMatrix!";
+  }
+  mvMatrix = mvMatrixStack.pop();
+}
+
+function degToRad(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+
+//
+// initGL
+//
+// Initialize WebGL, returning the GL context or null if
+// WebGL isn't available or could not be initialized.
+//
+function initGL(canvas) {
+  var gl = null;
+  try {
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+  } catch(e) {}
+
+  // If we don't have a GL context, give up now
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+  }
+  return gl;
+}
+
+//
+// getShader
+//
+// Loads a shader program by scouring the current document,
+// looking for a script with the specified ID.
+//
+
+
+function getShader(gl, id) {
+  var shaderScript = document.getElementById(id);
+
+  // Didn't find an element with the specified ID; abort.
+  if (!shaderScript) {
+    return null;
+  }
+
+  // Walk through the source element's children, building the
+  // shader source string.
+  var shaderSource = "";
+  var currentChild = shaderScript.firstChild;
+  while (currentChild) {
+    if (currentChild.nodeType == 3) {
+        shaderSource += currentChild.textContent;
+    }
+    currentChild = currentChild.nextSibling;
+  }
+  
+  // Now figure out what type of shader script we have,
+  // based on its MIME type.
+  var shader;
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;  // Unknown shader type
+  }
+
+  // Send the source to the shader object
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader program
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
+
+//
+// initShaders
+//
+// Initialize the shaders, so WebGL knows how to light our scene.
+//
+function initShaders() {
+  var fragmentShader = getShader(gl, "shader-fs");
+  var vertexShader = getShader(gl, "shader-vs");
+  
+  // Create the shader program
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  
+  // If creating the shader program failed, alert
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Unable to initialize the shader program.");
+  }
+  
+  // start using shading program for rendering
+  gl.useProgram(shaderProgram);
+  
+  // store location of aVertexPosition variable defined in shader
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+
+  // turn on vertex position attribute at specified position
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  // store location of aVertexColor variable defined in shader
+  //shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+
+  // turn on vertex color attribute at specified position
+  //gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+
+  // store location of uPMatrix variable defined in shader - projection matrix 
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+
+  // store location of uMVMatrix variable defined in shader - model-view matrix 
+  shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  
+  //VARIABLE za TEMP COLOR
+  shaderProgram.tempColor = gl.getUniformLocation(shaderProgram, "tempColor");
+}
+
+//
+// setMatrixUniforms
+//
+// Set the uniform values in shaders for model-view and projection matrix.
+//
+function setMatrixUniforms() {
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+//
+// initBuffers
+//
+// Initialize the buffers we'll need. For this demo, we have
+// two objecta -- a simple cube and pyramidß.
+//
+function initBuffers() {
+  // PYRAMID
+  // Create a buffer for the pyramid's vertices.
+  pyramidVertexPositionBuffer = gl.createBuffer();
+
+  // Select the pyramidVertexPositionBuffer as the one to apply vertex
+  // operations to from here out.
+  gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
+  var vertices = [
+    // Front face
+     0.0,  1.0,  0.0,
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+    // Right face
+     0.0,  1.0,  0.0,
+     1.0, -1.0,  1.0,
+     1.0, -1.0, -1.0,
+    // Back face
+     0.0,  1.0,  0.0,
+     1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0,
+    // Left face
+     0.0,  1.0,  0.0,
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0
+  ];
+
+  // Pass the list of vertices into WebGL to build the shape. We
+  // do this by creating a Float32Array from the JavaScript array,
+  // then use it to fill the current vertex buffer.
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  pyramidVertexPositionBuffer.itemSize = 3;
+  pyramidVertexPositionBuffer.numItems = 12;
+
+  // Now set up the colors for the vertices
+  pyramidVertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
+  var colors = [
+    // Front face
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    // Right face
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    // Back face
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    // Left face
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 1.0, 0.0, 1.0
+  ];
+
+  // Pass the colors into WebGL
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  pyramidVertexColorBuffer.itemSize = 4;
+  pyramidVertexColorBuffer.numItems = 3;
+
+  // CUBE
+  // Create a buffer for the cube's vertices.
+  cubeVertexPositionBuffer = gl.createBuffer();
+  
+  // Select the cubeVertexPositionBuffer as the one to apply vertex
+  // operations to from here out.
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+  
+  // Now create an array of vertices for the cube.
+  vertices = [
+    // Front face
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+     1.0,  1.0,  1.0,
+     1.0,  1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+
+    // Right face
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0,  1.0,  1.0,
+     1.0, -1.0,  1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0
+  ];
+  
+  // Now pass the list of vertices into WebGL to build the shape. We
+  // do this by creating a Float32Array from the JavaScript array,
+  // then use it to fill the current vertex buffer.
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  cubeVertexPositionBuffer.itemSize = 3;
+  cubeVertexPositionBuffer.numItems = 24;
+
+  // Now set up the colors for the vertices. We'll use solid colors
+  // for each face.
+  cubeVertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexColorBuffer);
+  colors = [
+      [1.0, 0.0, 0.0, 1.0], // Front face
+      [1.0, 1.0, 0.0, 1.0], // Back face
+      [0.0, 1.0, 0.0, 1.0], // Top face
+      [1.0, 0.5, 0.5, 1.0], // Bottom face
+      [1.0, 0.0, 1.0, 1.0], // Right face
+      [0.0, 0.0, 1.0, 1.0]  // Left face
+  ];
+
+  // Convert the array of colors into a table for all the vertices.
+  var unpackedColors = [];
+  for (var i in colors) {
+    var color = colors[i];
+
+    // Repeat each color four times for the four vertices of the face
+    for (var j=0; j < 4; j++) {
+          unpackedColors = unpackedColors.concat(color);
+      }
+  }
+
+  // Pass the colors into WebGL
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unpackedColors), gl.STATIC_DRAW);
+  cubeVertexColorBuffer.itemSize = 4;
+  cubeVertexColorBuffer.numItems = 24;
+
+  // Build the element array buffer; this specifies the indices
+  // into the vertex array for each face's vertices.
+  cubeVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+
+  // This array defines each face as two triangles, using the
+  // indices into the vertex array to specify each triangle's
+  // position.
+  var cubeVertexIndices = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+  ];
+
+  // Now send the element array to GL
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+  cubeVertexIndexBuffer.itemSize = 1;
+  cubeVertexIndexBuffer.numItems = 36;
+}
+
+var kSpeed = 10;
+function drawScene() {
+	//NUJNO ZLO
+	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	//PERSPEKTIVA
+	// Establish the perspective with which we want to view the
+	// scene. Our field of view is 45 degrees, with a width/height
+	// ratio and we only want to see objects between 0.1 units
+	// and 100 units away from the camera.
+	mat4.perspective(90, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+	//mat4.identity(pMatrix);
+	//mat4.ortho(0, 1280, 0, 720, 0.01, 1000.0, pMatrix);
+	
+	//POZICIJA KAMERE
+	mat4.identity(mvMatrix);
+	
+	//mat4.translate(mvMatrix, [0.0, -100.0, 0.0]);
+	mat4.rotate(mvMatrix, degToRad(45), [1, 0, 0]);
+	mat4.translate(mvMatrix, [-xPos, -10.0, yPos]);
+	//console.log(mvMatrix);
+
+
+
+	//////RISANJE OBJEKTOV V SVETU
+	hero.draw();
+	world.draw();
+}
+
+
+
+
+///////HANDLE KEYS
+
+function handleKeyDown(event) {
+  // storing the pressed state for individual key
+  currentlyPressedKeys[event.keyCode] = true;
+}
+
+function handleKeyUp(event) {
+  // reseting the pressed state for individual key
+  currentlyPressedKeys[event.keyCode] = false;
+}
+
+
+function handleKeys() {
+
+  if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+    // Left cursor key or A
+    speedX = -0.03*kSpeed;
+  } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+    // Right cursor key or D
+    speedX = 0.03*kSpeed;
+  } else {
+    speedX = 0;
+  }
+
+  if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
+    // Up cursor key or W
+    speedY = 0.03*kSpeed;
+  } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
+    // Down cursor key
+    speedY = -0.03*kSpeed;
+  } else {
+    speedY = 0;
+  }
+}
+
+
+
+
+//
+// animate
+//
+// Called every time before redeawing the screen.
+//
+function animate() {
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+
+    // rotate pyramid and cube for a small amount
+    rotationPyramid += (90 * elapsed) / 1000.0;
+    rotationCube += (75 * elapsed) / 1000.0;
+	//console.log(speed);
+	xPos += speedX;
+	yPos += speedY;
+	
+  }
+  lastTime = timeNow;
+}
+
+//
+// start
+//
+// Called when the canvas is created to get the ball rolling.
+//
+function start() {
+  canvas = document.getElementById("glcanvas");
+
+  gl = initGL(canvas);      // Initialize the GL context
+
+  // Only continue if WebGL is available and working
+  if (gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
+    gl.clearDepth(1.0);                                     // Clear everything
+    gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
+
+    // Initialize the shaders; this is where all the lighting for the
+    // vertices and so forth is established.
+    initShaders();
+    
+    // Here's where we call the routine that builds all the objects
+    // we'll be drawing.
+    //initBuffers();
+	
+	
+	
+	setTimeout(function(){
+		world = new World();
+		world.load();
+	}, 0);
+	
+	setTimeout(function(){
+		hero = new Hero();
+		hero.load();
+	}, 0);
+
+	
+	
+	
+	document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+	
+	//console.log(hero.vertexPositionBuffer)
+    
+    // Set up to draw the scene periodically.
+    setInterval(function() {
+      requestAnimationFrame(animate);
+	  handleKeys();
+      drawScene();
+    }, 15);
+  }
+}
---
+++ scripts/world.js
@@ -0,0 +1,124 @@
+function World() {
+	this.vertexPositionBuffer = null;
+	this.vertexIndexBuffer = null;
+	//this.vertexTextureCoordBuffer;
+	
+	this.vec4Color = [1.0, 1.0, 1.0, 1.0];
+}
+World.prototype.load = function() {
+    var request = new XMLHttpRequest();
+	request.open("GET", "./assets/world_plane.obj");
+	request.onreadystatechange = function () {
+		if (request.readyState == 4) {
+			handleLoadedWorld(request.responseText);
+		}
+	}
+	request.send();
+	
+	
+	var handleLoadedWorld = function(data) {
+	  var lines = data.split("\n");
+	  var vertexCount = 0;
+	  var vertexPositions = [];
+	  var vertexTextureCoords = [];
+	  var vertexF = [];
+	  var indexCount = 0;
+	  for (var i in lines) {
+		var vals = lines[i].split(" ");
+		if (vals.length == 4 && vals[0] == "v") {
+		  // It is a line describing a vertex; get X, Y and Z first
+		  vertexPositions.push(parseFloat(vals[1]));
+		  vertexPositions.push(parseFloat(vals[2]));
+		  vertexPositions.push(parseFloat(vals[3]));		
+		  vertexCount += 1;
+		} else if (vals.length == 4 && vals[0] == "f") {
+			vertexF.push(parseInt(vals[1]) - 1);
+			vertexF.push(parseInt(vals[2]) - 1);
+			vertexF.push(parseInt(vals[3]) - 1);
+			indexCount += 3;
+		}
+	  }
+	  
+	  
+	  world.vertexPositionBuffer = gl.createBuffer();
+	  gl.bindBuffer(gl.ARRAY_BUFFER, world.vertexPositionBuffer);
+	  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositions), gl.STATIC_DRAW);
+	  world.vertexPositionBuffer.itemSize = 3;
+	  world.vertexPositionBuffer.numItems = vertexCount;
+	  
+	  
+	   /*var cubeVertexIndices = [
+		  0, 1, 2,      0, 2, 3,    // Front face
+		  4, 5, 6,      4, 6, 7,    // Back face
+		  8, 9, 10,     8, 10, 11,  // Top face
+		  12, 13, 14,   12, 14, 15, // Bottom face
+		  16, 17, 18,   16, 18, 19, // Right face
+		  20, 21, 22,   20, 22, 23  // Left face
+	  ];*/
+
+	  // Now send the element array to GL
+	  world.vertexIndexBuffer = gl.createBuffer();
+	  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, world.vertexIndexBuffer);
+	  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexF), gl.STATIC_DRAW);
+	  world.vertexIndexBuffer.itemSize = 1;
+	  world.vertexIndexBuffer.numItems = indexCount;
+
+	}
+	
+};
+
+World.prototype.draw = function() {
+	gl.uniform4f(
+      shaderProgram.tempColor,
+      this.vec4Color[0],
+      this.vec4Color[1],
+	  this.vec4Color[2],
+	  this.vec4Color[3]
+    );
+	// Activate textures
+	 mvPushMatrix();
+	 
+  //ZA IZOMETRIČNO
+  //mat4.scale(mvMatrix, [50.0,0.0,50.0]);
+  
+  
+  mat4.translate(mvMatrix, [0.0, -10.0, 0.0]);
+  /*gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, wallTexture);
+  gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+  // Set the texture coordinates attribute for the vertices.
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTextureCoordBuffer);
+  gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, vertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);*/
+
+  // Draw the world by binding the array buffer to the world's vertices
+  // array, setting attributes, and pushing it to GL.
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  
+  // Set the colors attribute for the vertices.
+  /*gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexColorBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, cubeVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);*/
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+
+  // Draw the cube.
+  setMatrixUniforms();
+  gl.drawElements(gl.TRIANGLES, this.vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+
+  // Restore the original matrix
+  mvPopMatrix();
+}
+
+//
+// handleLoadedWorld
+//
+// Initialisation of world 
+//
+
+
+//
+// loadWorld
+//
+// Loading world 
+//
